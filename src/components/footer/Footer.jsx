f39bbc8fcabbcbf1db6cfb36f78ebc305ba421c1
Sneha <snehalall.sneha@gmail.com>
2023-03-01 11:49:47 +0530

    feat:create footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './footer.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer_content">
+        THEMES
+        <button className="theme1">1</button>
+        <button className="theme2">2</button>
+        <button className="theme3">3</button>
+      </div>
+    </div>
+  );
+}
